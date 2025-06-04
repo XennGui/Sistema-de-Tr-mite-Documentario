66e7d810491ef459f0dd987ca2e6ac3b7b27ee4f
XennGui <xen.luna07@gmail.com>
2025-06-03 21:08:56 -0500

    Requirement update.

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from rutas.area import router as area_router
+from rutas.usuario import router as usuario_router
 from pydantic import BaseModel
 from typing import Optional, List, Tuple
 from embeddings import create_embeddings, save_vectorstore, load_vectorstore, prepare_docs_from_db
@@ -24,6 +25,7 @@ app.add_middleware(
 )
 
 app.include_router(area_router)
+app.include_router(usuario_router)
 
 # Carga y configuración del modelo
 vectorstore = load_vectorstore()
